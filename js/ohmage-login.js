0a3e7991f7f42fcad11315e91c7764b8c355981d
AlphaO <alphao@ubuntu.(none)>
2012-11-03 14:31:39 -0700

    Added campaign page and login auth

---
+++ js/ohmage-login.js
@@ -0,0 +1,17 @@
+$(function() {
+    $("#login-form").submit(function() {
+        var $this = $(this);
+        var inputUsername = $this.find("#inputUsername").val();
+        var inputPassword = $this.find("#inputPassword").val();
+        $.post("https://test.ohmage.org/app/user/auth_token", { user: inputUsername, password: inputPassword, client: "campaign-webapp" }, function(data) {
+            var authResponse = $.parseJSON(data);
+            if (authResponse.result == "success") {
+                $.cookie("auth_token", authResponse.token);
+            }
+            else {
+                $('<div class="alert alert-error">WRONG PASSWORD, YO</div>').prependTo('.content');
+            }
+        });
+        return false;
+    });
+});
