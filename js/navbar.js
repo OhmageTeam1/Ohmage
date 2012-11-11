e5e2b5d4040f4f4634af668073b7c037d1853d86
Tai Pham <taiducpham@ucla.edu>
2012-11-11 11:05:45 -0800

    add aconfirm box when log out

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,13 @@
 $(function() {
    $('#username').html("Logged in as " + $.cookie('username'));
    $('.logout-button').click(function () {
-        $.post("https://test.ohmage.org/app/user/logout", { client: "campaign-webapp", auth_token: $.cookie('authToken')});
-        $.removeCookie('authToken');
-        $.removeCookie('username');
-        window.location.replace('login.php');
+		var r = window.confirm("All unsaved data will be lost");
+		if (r == true)
+		{
+			$.post("https://test.ohmage.org/app/user/logout", { client: "campaign-webapp", auth_token: $.cookie('authToken')});
+			$.removeCookie('authToken');
+			$.removeCookie('username');
+			window.location.replace('login.php');
+		}
    });
 });
