cdc4e06d7b701f26e91d50b974331e802289ad1f
Allen Zhang <xalphaox@gmail.com>
2012-11-27 03:33:12 -0800

    syntax changes

--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
     <div class="navbar-inner">
         <div class="navbar-container">
             <a class="brand" href="#"><img src="img/ohmage-logo.png" width="112"></a>
-            <button type="button" class="logoutButton pull-right btn btn-info">Log Out</button>
+            <a href="login.php" class="logoutButton pull-right btn btn-info">Log Out</a>
             <p class="navbar-text pull-right" id="username"></p>
         </div>
     </div>
